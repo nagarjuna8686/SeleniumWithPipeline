059314f07b207b9282dc963d33116c4d09d14f43
shubhams <shubhamksaxena@gmail.com>
2018-04-05 18:55:02 +0530

    Created Automation Project

---
+++ E2ETesting/src/com/e2etesting/TC_001.java
@@ -0,0 +1,41 @@
+package com.e2etesting;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+
+	@Test
+	
+	public void loginTest()
+	{
+	
+	System.setProperty("webdriver.chrome.driver","D:\\Drivers\\Chrome\\chromedriver.exe");
+	ChromeDriver driver=new ChromeDriver();
+	
+	driver.get("https://test.salesforce.com/");
+	
+	driver.manage().window().maximize();
+	
+	driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+	
+	driver.findElement(By.id("username")).sendKeys("dsintegration-admin@servicesource.com.sitdelemea");
+	  
+	  driver.findElement(By.id("password")).sendKeys("srevds123!");
+	  
+	  driver.findElement(By.id("Login")).click();
+	  
+	  driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+	  
+	  String pTitle=driver.getTitle();
+	  
+	  driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+	
+	  System.out.println("Home Page Title is ::::" +pTitle);
+	
+	driver.quit();
+	}
+}
