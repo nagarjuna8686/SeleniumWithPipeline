c8ff657110728d1ad7265fbdaca497fc3a5881d6
shubhams <shubhams@ShubhamS.positiveedge.com>
2018-04-05 19:58:12 +0530

    Test Changes

---
+++ E2ETesting/src/com/e2etesting/NewTest.java
@@ -0,0 +1,11 @@
+package com.e2etesting;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void createnew() {
+	  
+	  System.out.println("this is just code");
+  }
+}
